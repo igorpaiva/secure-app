86d5776a3b2ba3e6a4a63ca998ba002739dae7be
igor.araujo <igor.araujo@virtus.ufcg.edu.br>
2023-05-23 23:35:19 -0300

    [back] - add backend

---
+++ spring-secured/src/main/java/com/springsecured/SpringSecuredApplication.java
@@ -0,0 +1,13 @@
+package com.springsecured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuredApplication.class, args);
+	}
+
+}
---
+++ spring-secured/src/test/java/com/springsecured/SpringSecuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
