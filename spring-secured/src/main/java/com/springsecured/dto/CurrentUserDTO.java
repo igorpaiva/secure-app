287e89e805ed1cdf5af053089a0abc0f87d59c92
igorpaiva <igor.araujo@ee.ufcg.edu.br>
2023-05-29 17:17:10 -0300

    [front] - add login and home pages

--- spring-secured/src/main/java/com/springsecured/dto/CurrentUserDTO.java
+++ spring-secured/src/main/java/com/springsecured/dto/CurrentUserDTO.java
@@ -3,8 +3,7 @@ package com.springsecured.dto;
 import lombok.Data;
 
 @Data
-//@AllArgsConstructor
-public class CurrentUserDTO extends ModelDTO{
+public class CurrentUserDTO{
     private String username;
     private String password;
 }
