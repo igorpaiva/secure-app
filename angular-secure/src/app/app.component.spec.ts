d6f57df794eebfe17db62437b97908e58e507d12
igorpaiva <igor.araujo@ee.ufcg.edu.br>
2023-06-01 11:02:50 -0300

    [infra] - rename projects

--- angular-secure/src/app/app.component.spec.ts
+++ angular-secure/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-secured'`, () => {
+  it(`should have as title 'angular-secure'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-secured');
+    expect(app.title).toEqual('angular-secure');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-secured app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-secure app is running!');
   });
 });
